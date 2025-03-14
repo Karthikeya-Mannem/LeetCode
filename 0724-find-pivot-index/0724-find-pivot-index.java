9a9d8610ed1ebe4841f725aa1855f6dd48cbe7e9
Karthikeya Mannem <mannemkarthikeya99@gmail.com>
2025-03-14 22:01:13 +0530

    Time: 1 ms (93.53%), Space: 45.6 MB (54.11%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int ts = 0;
+        for(int i = 0;i<nums.length;i++){
+            ts += nums[i];
+        }
+        int ls = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(ls == ts-ls-nums[i]){
+                return i;
+            }
+            ls += nums[i];
+        }
+        return -1;
+    }
+}
